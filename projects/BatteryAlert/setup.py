bbc590f81db912f75cd870952482a5d4bc1cc9a3
narm2005 <36932584+narm2005@users.noreply.github.com>
2018-06-11 15:18:47 -0400

    project files

---
+++ projects/BatteryAlert/setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import sys
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+setup(name='BatteryAlert',
+      version='1.1.8',
+      install_requires=[
+          r for r in open('requirements.txt', 'r').read().split('\n') if r],
+      packages=['balert', ],
+      entry_points={
+          'console_scripts': ['balert=balert:main'],
+      },
+      description="A speaking battery's charge level",
+      long_description=open('README').read(),
+      keywords=['reminder', 'battery',
+                'notification', 'voice alert', 'python'],
+      classifiers=[
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: System :: Monitoring'
+      ],
+      )
